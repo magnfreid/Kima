3ad680be7ed6f89d75605d32607b0cb10967e33d
Magnus Freidenfelt <sabbathmagnus@gmail.com>
2024-11-19 15:27:31 +0100

    Initial commit.
    Added MainActivity.kt, GameActivity.kt, HandOfCardsFragment.kt + PlayedCardFragment.kt

---
+++ app/src/main/java/com/example/kima/GameActivity.kt
@@ -0,0 +1,23 @@
+package com.example.kima
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.kima.databinding.ActivityGameBinding
+
+class GameActivity : AppCompatActivity() {
+    lateinit var binding : ActivityGameBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityGameBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
