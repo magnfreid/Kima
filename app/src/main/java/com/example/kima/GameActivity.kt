fe1e43de2ad7fb973e5959595e706cc5ea095bbc
Kim Bellander <kim.bellander@gmail.com>
2024-11-21 22:06:58 +0100

    Updated Card-class since I couldn't use Card-objects for the bundle.
    Created functions in GameActivity for setting card hands (user and computer).
    
    Sent userHand as bundle to HandOfCardFragment to be able to be displayed in fragment.

--- app/src/main/java/com/example/kima/GameActivity.kt
+++ app/src/main/java/com/example/kima/GameActivity.kt
@@ -13,40 +13,71 @@ import com.example.kima.models.Deck
 class GameActivity : AppCompatActivity() {
     lateinit var binding: ActivityGameBinding
     val deck: MutableList<Card> = Deck().generateDeck()
+    val userHand : MutableList<Card> = mutableListOf()
+    val computerHand : MutableList<Card> = mutableListOf()
 
-    lateinit var ivPresentComputerCard: ImageView
+//    lateinit var ivPresentComputerCard: ImageView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        drawFullHand(userHand)
+        drawFullHand(computerHand)
+
         super.onCreate(savedInstanceState)
+
         binding = ActivityGameBinding.inflate(layoutInflater)
+
         enableEdgeToEdge()
+
         setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        ivPresentComputerCard = findViewById(R.id.present_computer_card)
+        showHandFragment()
 
-        val randomIndex = randomiseCard()
+        /*ivPresentComputerCard = findViewById(R.id.present_computer_card)
+
+        val randomIndex = randomiseCard(deck)
         val randomCard = deck[randomIndex]
         val nameFromCard = randomCard.imageName
         val idFromCard = resources.getIdentifier(nameFromCard, "drawable", packageName)
         ivPresentComputerCard.setImageResource(idFromCard)
-        showHandFragment()
+        showHandFragment()*/
 
 
     }
 
-    fun randomiseCard(): Int {
-        return (0..47).random()
+    private fun randomiseCard(deck : MutableList<Card>) : Int {
+        return (0 until deck.size).random()
+    }
+
+    private fun drawCardUser(deck: MutableList<Card>, hand: MutableList<Card>) {
+        val randomIndex = randomiseCard(deck)
+        val randomCard = deck[randomIndex]
+        deck.removeAt(randomIndex)
+        hand.add(randomCard)
+    }
+
+
+
+    private fun drawFullHand(hand: MutableList<Card>) {
+        for(i in 1..5) {
+            drawCardUser(deck, hand)
+        }
     }
 
     private fun showHandFragment() {
+        val bundle = Bundle()
+        bundle.putParcelableArrayList("userHand", ArrayList(userHand))
+        val fragment = HandOfCardsFragment()
+        fragment.arguments = bundle
         supportFragmentManager.beginTransaction().apply {
-            replace(binding.fcvPlayer.id, HandOfCardsFragment())
+            replace(binding.fcvPlayer.id, fragment)
             commit()
         }
     }
--- app/src/main/java/com/example/kima/HandOfCardsFragment.kt
+++ app/src/main/java/com/example/kima/HandOfCardsFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.kima.controllers.HandAdapter
@@ -18,6 +19,17 @@ import com.example.kima.models.Card
  * create an instance of this fragment.
  */
 class HandOfCardsFragment : Fragment() {
+    val userHandView : MutableList<ImageView> = mutableListOf()
+
+    fun displayUserHand(userHandView: MutableList<ImageView>, userHand : MutableList<Card>) {
+        for(i in  0..userHand.size-1) {
+            val cardName = userHand[i].imageName
+            val imageResId = resources.getIdentifier(cardName, "drawable", requireContext().packageName)
+            userHandView[i].setImageResource(imageResId)
+        }
+
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -27,18 +39,44 @@ class HandOfCardsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val recyclerView = view.findViewById<RecyclerView>(R.id.rv_hand)
-        recyclerView.adapter = HandAdapter(testList)
-      recyclerView.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+
+
+
+
+
+        val ivUserCard1 = view.findViewById<ImageView>(R.id.iv_user_card_1)
+        val ivUserCard2 = view.findViewById<ImageView>(R.id.iv_user_card_2)
+        val ivUserCard3 = view.findViewById<ImageView>(R.id.iv_user_card_3)
+        val ivUserCard4 = view.findViewById<ImageView>(R.id.iv_user_card_4)
+        val ivUserCard5 = view.findViewById<ImageView>(R.id.iv_user_card_5)
+
+        userHandView.add(ivUserCard1)
+        userHandView.add(ivUserCard2)
+        userHandView.add(ivUserCard3)
+        userHandView.add(ivUserCard4)
+        userHandView.add(ivUserCard5)
+
+        val userHand = arguments?.getParcelableArrayList<Card>("userHand") as? MutableList<Card> ?: mutableListOf()
+        displayUserHand(userHandView, userHand)
+
+        for(imageView in userHandView) {
+            imageView.setOnClickListener {
+                imageView.animate().translationY(-50f).setDuration(150)
+            }
+        }
+
+//        val recyclerView = view.findViewById<RecyclerView>(R.id.rv_hand)
+//        recyclerView.adapter = HandAdapter(testList)
+//      recyclerView.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
 
     }
 
-    private val testList = mutableListOf(
+    /*private val testList = mutableListOf(
         Card("diamonds", 3, R.drawable.diamonds_3, "diamonds_3"),
         (Card("diamonds", 4, R.drawable.diamonds_4, "diamonds_4")),
         (Card("diamonds", 5, R.drawable.diamonds_5, "diamonds_5")),
         (Card("diamonds", 6, R.drawable.diamonds_6, "diamonds_6")),
         (Card("diamonds", 7, R.drawable.diamonds_7, "diamonds_7"))
-    )
+    )*/
 
 }
