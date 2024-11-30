dd47cbe5e583dd8ab7eac0d7de0a63772e3a00f1
Kim Bellander <kim.bellander@gmail.com>
2024-11-30 16:58:49 +0100

    Linked checkWinner() to the popup to display the correct winner. (Had to make many changes to the existing code, so duplicated it and commented out the existing code. Made the changes in the duplicate.

--- app/src/main/java/com/example/kima/RulesModule.kt
+++ app/src/main/java/com/example/kima/RulesModule.kt
@@ -1,15 +1,18 @@
 package com.example.kima
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.kima.models.Player
 
-class RulesModule(viewModel: GameViewModel) : GameLogics {
+/*class RulesModule(viewModel: GameViewModel) : GameLogics {
 
 
     private val player = Player("You", viewModel.userCard.value)
     private val computer = Player("Computer", viewModel.userCard.value)
     private var activePlayer = player
     private var winner = activePlayer
+
 
     override fun checkWinner() {
         val reactivePlayer = if (activePlayer == player) computer else player
@@ -29,6 +32,52 @@ class RulesModule(viewModel: GameViewModel) : GameLogics {
     override fun resolveTurn() {
         activePlayer = winner
         winner.score++
+    }
+
+
+}*/
+
+class RulesModule(viewModel: GameViewModel) {
+
+
+    private val player = Player("You", viewModel.userCard.value)
+    private val computer = Player("Computer", viewModel.computerCard.value)
+    private var activePlayer = player
+
+    private val _winner= MutableLiveData<Player>()
+    val winner: LiveData<Player> get() = _winner
+
+    init {
+        viewModel.userCard.observeForever { card ->
+            player.playedCard = card
+            checkWinner()
+        }
+        viewModel.computerCard.observeForever { card ->
+            computer.playedCard = card
+            checkWinner()
+        }
+    }
+
+    fun checkWinner() {
+        _winner.value = activePlayer
+        val reactivePlayer = if (activePlayer == player) computer else player
+        if (activePlayer.playedCard != null && reactivePlayer.playedCard != null) {
+            if (reactivePlayer.playedCard?.suit == activePlayer.playedCard?.suit) {
+                if (reactivePlayer.playedCard?.rank!! > activePlayer.playedCard?.rank!!) {
+                    _winner.value = reactivePlayer
+                }
+            }
+            //TODO Hantera på bättre sätt?
+        } else Log.d(
+            "SOUT",
+            "Card error: Active player card: ${activePlayer.playedCard} - Reactive player card: ${reactivePlayer.playedCard}"
+        )
+    }
+
+    fun resolveTurn() {
+        activePlayer = _winner.value!!
+        activePlayer.score++
+        _winner.value = activePlayer
     }
 
 
--- app/src/main/java/com/example/kima/TrickDialogFragment.kt
+++ app/src/main/java/com/example/kima/TrickDialogFragment.kt
@@ -5,9 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.DialogFragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.kima.models.Player
 
 class TrickDialogFragment : DialogFragment() {
+
+    lateinit var vm: GameViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        vm = ViewModelProvider(requireActivity()).get(GameViewModel::class.java)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,11 +30,20 @@ class TrickDialogFragment : DialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         val btnNextTrick = view.findViewById<Button>(R.id.btn_next_trick)
+        val tvDisplayTrickWinner = view.findViewById<TextView>(R.id.tv_display_trick_winner)
+
+
+        vm.winner.observe(viewLifecycleOwner) { newWinner ->
+            tvDisplayTrickWinner.text = "${newWinner.name} won!"
+        }
+
+        vm.checkWinner()
+
         btnNextTrick.setOnClickListener {
             dialog?.dismiss()
         }
-
 
     }
 }
