35459ccde3ebadd890d49897ac1a7e6da82b7834
Magnus Freidenfelt <sabbathmagnus@gmail.com>
2024-12-05 11:27:52 +0100

    - Updated the text for the rules popup.

--- app/src/main/java/com/example/kima/views/RulesDialogFragment.kt
+++ app/src/main/java/com/example/kima/views/RulesDialogFragment.kt
@@ -4,14 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.text.HtmlCompat
 import androidx.fragment.app.DialogFragment
 import com.example.kima.R
 import com.google.android.material.button.MaterialButton
+import com.google.android.material.textview.MaterialTextView
 
 /**
  * A popup displaying the rules of the game.
  */
 class RulesDialogFragment : DialogFragment() {
+    lateinit var tvInfoText: MaterialTextView
+    lateinit var btnCloseDialog: MaterialButton
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,8 +27,10 @@ class RulesDialogFragment : DialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val btnCloseView = view.findViewById<MaterialButton>(R.id.btn_close_dialog)
-        btnCloseView.setOnClickListener {
+       btnCloseDialog = view.findViewById(R.id.btn_close_dialog)
+        tvInfoText = view.findViewById(R.id.tv_info)
+        tvInfoText.text = getString(R.string.rules_dialog_info_text).trimIndent()
+        btnCloseDialog.setOnClickListener {
             dialog?.dismiss()
         }
     }
