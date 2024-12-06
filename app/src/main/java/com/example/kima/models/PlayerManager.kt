82a7b8f6bcadb76a44c3c4dfc468e594fb92098d
Kim Bellander <kim.bellander@gmail.com>
2024-12-06 16:22:09 +0100

    Remedy some yellow warnings.

--- app/src/main/java/com/example/kima/models/PlayerManager.kt
+++ app/src/main/java/com/example/kima/models/PlayerManager.kt
@@ -1,6 +1,5 @@
 package com.example.kima.models
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 
--- app/src/main/java/com/example/kima/views/ScoreboardDialogFragment.kt
+++ app/src/main/java/com/example/kima/views/ScoreboardDialogFragment.kt
@@ -4,7 +4,6 @@ import android.app.Dialog
 import android.content.Context
 import android.graphics.Typeface
 import android.os.Bundle
-import android.util.Log
 import android.view.ContextThemeWrapper
 import android.view.Gravity
 import android.view.LayoutInflater
